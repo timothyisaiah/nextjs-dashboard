5e9b93df21a60267785fdc6d7538dfe44029dce7
timothyisaiah <timothyisaiah7@gmail.com>
2025-04-05 13:03:05 +0300

    Learning NextJs

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold">Invoices</h1>
+      <p className="mt-4 text-lg">Welcome to the Invoices Page!</p>
+    </div>
+  );
+}
